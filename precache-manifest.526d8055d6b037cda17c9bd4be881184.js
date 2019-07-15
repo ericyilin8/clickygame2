cdd8badb350367f1d95f5e35daa430b54d8694be
ericyilin8 <ericyilin8@gmail.com>
2019-07-14 23:17:30 -0400

    Updates

---
+++ precache-manifest.526d8055d6b037cda17c9bd4be881184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "7951c603d1dce9167ed5",
+    "url": "/clickygame/static/js/main.c542dc66.chunk.js"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/clickygame/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "7951c603d1dce9167ed5",
+    "url": "/clickygame/static/css/main.aebf1272.chunk.css"
+  },
+  {
+    "revision": "f238ee21ff1befdaec44daa383dbf47b",
+    "url": "/clickygame/index.html"
+  }
+];
